e1922ff1ce3415c650a07d97f1fe90ec59ea780b
thewronghand <penfreak77@gmail.com>
2024-11-14 19:00:22 +0900

    feat: firebase admin SDK 초기화 함수 구현

--- functions/src/config/index.ts
+++ functions/src/config/index.ts
@@ -5,3 +5,5 @@ export const kakaoConfig = {
   restApiKey: process.env.KAKAO_REST_API_KEY || '',
   redirectUri: process.env.KAKAO_REDIRECT_URI || '',
 };
+
+export { initializeFirebaseAdmin } from './firebase';
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,14 @@
-import * as functions from 'firebase-functions';
+import * as functions from 'firebase-functions/v2';
+import { initializeFirebaseAdmin } from './config';
 import app from './app';
 
-// auth 함수 export
-export const auth = functions.https.onRequest(app);
+// Firebase Admin 초기화
+initializeFirebaseAdmin();
+
+// Secret Manager를 사용하도록 설정
+export const auth = functions.https.onRequest(
+  {
+    secrets: ['SERVICE_ACCOUNT_KEY'],
+  },
+  app
+);
