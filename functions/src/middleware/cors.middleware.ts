f68027364f3d8db681926c6bf2a130fdeb7a6d7d
thewronghand <penfreak77@gmail.com>
2025-01-22 23:21:36 +0900

    chore: 배포된 클라이언트 주소 cors 설정 추가

--- functions/src/middleware/cors.middleware.ts
+++ functions/src/middleware/cors.middleware.ts
@@ -2,7 +2,11 @@ import * as cors from 'cors';
 import { RequestHandler } from 'express';
 
 export const corsMiddleware: RequestHandler = cors({
-  origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+  origin: [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    'https://moonstruck-b17d6.web.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true,
